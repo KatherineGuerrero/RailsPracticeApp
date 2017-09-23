d40badbb1e8b7b89c3c7d201cda3e76048caf289
KatherineGuerrero <kfguerrero@uc.cl>
2017-09-22 23:15:13 -0300

    StaticPages controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170919012620) do
+ActiveRecord::Schema.define(version: 20170919014630) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
-    t.date "creation_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
